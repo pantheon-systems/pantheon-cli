ed996758b97ea5c7aa31a0886a43c937b182ad59
Brian Weaver <brian.weaver@pantheon.io>
2021-09-09 16:09:45 -0400

    SSHKeyCommandsTest.php - SPACES AROUND IF() OMG

--- tests/Functional/SSHKeyCommandsTest.php
+++ tests/Functional/SSHKeyCommandsTest.php
@@ -44,7 +44,7 @@ class SSHKeyCommandsTest extends TestCase
         echo "New ID list: " . print_r($new_id_list) . "\n";
         $new_key = array_diff($new_id_list, $original_id_list);
         echo "New key: " . print_r($new_key, true) . "\n";
-        if(is_array($new_key)) {
+        if (is_array($new_key)) {
             $new_key = array_pop($new_key);
         }
 
